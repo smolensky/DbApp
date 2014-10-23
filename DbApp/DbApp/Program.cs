f84399a07bbb1f4e773bf8775ec7bd104e284cf8
Bohdan <te.dark@mail.ru>
2014-10-23 14:10:54 -0700

    made input from console

---
+++ DbApp/DbApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace DbApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Input first name:");
+            var firstName = Console.ReadLine();
+
+            Console.WriteLine("Input second name:");
+            var secondName = Console.ReadLine();
+
+            Console.WriteLine("Choose your faculty:");
+            var facultyId = Console.ReadLine();
+
+            Console.WriteLine("Input overal mark:");
+            var overalMark = Console.ReadLine();
+
+            Console.WriteLine("Hello " + firstName + secondName);
+            Console.WriteLine("Your faculty is " + facultyId);
+            Console.WriteLine("Your overal mark is " + overalMark);
+
+            Console.ReadKey();
+        }
+    }
+
+    public class StudentsRepo
+    {
+        
+    }
+
+    public class StudentsDto
+    {
+        
+    }
+}
